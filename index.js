90a9e54dc34969e659d3f4096d9974e9c07e9ef4
chris dostert <chris.dostert@nintex.com>
2017-06-17 12:57:40 -0700

    chore) update error handling

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const msRestAzure = require('ms-rest-azure');
 const AzureArmSb = require('azure-arm-sb');
+const process = require('process');
 
 const login = async () => {
+    console.log('logging in');
+
     const loginType = process.env.loginType;
     const loginId = process.env.loginId;
     const loginSecret = process.env.loginSecret;
-    const loginOpts = { domain: process.env.loginTenantId};
+    const loginOpts = {domain: process.env.loginTenantId};
 
+    let response;
     if (loginType === 'sp') {
         // https://github.com/Azure/azure-sdk-for-node/blob/master/runtime/ms-rest-azure/index.d.ts#L397
-        return msRestAzure.loginWithServicePrincipalSecret(loginId, loginSecret, loginOpts);
+        response = await msRestAzure.loginWithServicePrincipalSecret(loginId, loginSecret, loginOpts);
     } else {
         // https://github.com/Azure/azure-sdk-for-node/blob/master/runtime/ms-rest-azure/index.d.ts#L356
-        return msRestAzure.loginWithUsernamePassword(loginId, loginSecret, loginOpts);
+        response = await msRestAzure.loginWithUsernamePassword(loginId, loginSecret, loginOpts);
     }
-};
 
-console.log('logging in');
-credentials = login().then(credentials => {
     console.log('login successful');
 
+    return response;
+};
+
+const createOrUpdate = async (credentials) => {
+    console.log('creating/updating namespace');
     const azureArmSb = new AzureArmSb(credentials, process.env.subscriptionId);
 
     // see https://github.com/Azure/azure-sdk-for-node/blob/master/lib/services/serviceBusManagement2/lib/operations/namespaces.js#L3234
@@ -32,16 +38,16 @@ credentials = login().then(credentials => {
         }
     };
 
-    console.log('creating/updating namespace');
-    azureArmSb.namespaces.createOrUpdate(
+    await azureArmSb.namespaces.createOrUpdate(
         process.env.resourceGroup,
         process.env.name,
         options,
-        error => {
-            if (error) {
-                throw error;
-            }
-            console.log('creating/updating namespace successful');
-        }
     );
+
+    console.log('creating/updating namespace successful')
+};
+
+login().then(createOrUpdate).catch(error => {
+    console.log(error);
+    process.exit(1)
 });
